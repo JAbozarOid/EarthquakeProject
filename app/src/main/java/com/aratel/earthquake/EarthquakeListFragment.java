242ead9641c4af5502f1f1d14844a3b98c6dd58a
JAbozarOid <araghibdoust@hotmail.com>
2020-04-14 18:23:09 +0430

    complete dummy earthquake list

--- app/src/main/java/com/aratel/earthquake/EarthquakeListFragment.java
+++ app/src/main/java/com/aratel/earthquake/EarthquakeListFragment.java
@@ -1,5 +1,6 @@
 package com.aratel.earthquake;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +9,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeListFragment extends Fragment {
 
     private ArrayList<Earthquake> mEarthquake = new ArrayList<Earthquake>();
+    private RecyclerView mRecyclerView;
+    private EarthquakeRecyclerViewAdapter mEarthquakeAdapter = new EarthquakeRecyclerViewAdapter(mEarthquake);
 
     public EarthquakeListFragment() {
     }
@@ -26,7 +32,29 @@ public class EarthquakeListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_earthquake_list,container,false);
+        View view = inflater.inflate(R.layout.fragment_earthquake_list, container, false);
+        mRecyclerView = view.findViewById(R.id.list);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Set the Recycler View adapter
+        Context context = view.getContext();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        mRecyclerView.setAdapter(mEarthquakeAdapter);
+    }
+
+    public void setEarthquakes(List<Earthquake> earthquakes) {
+        for (Earthquake earthquake : earthquakes
+        ) {
+            // check for duplicates
+            if (!mEarthquake.contains(earthquake)) {
+                mEarthquake.add(earthquake);
+                mEarthquakeAdapter.notifyItemInserted(earthquakes.indexOf(earthquake));
+            }
+        }
     }
 }
