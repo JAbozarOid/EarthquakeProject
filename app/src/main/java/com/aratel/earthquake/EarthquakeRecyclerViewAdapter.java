0e92077ce0f3194be06b978f39e5c0d8bb90d7b6
JAbozarOid <araghibdoust@hotmail.com>
2020-05-12 18:12:33 +0430

    improve adapter&list_item_earthquake-99.02.23

--- app/src/main/java/com/aratel/earthquake/EarthquakeRecyclerViewAdapter.java
+++ app/src/main/java/com/aratel/earthquake/EarthquakeRecyclerViewAdapter.java
@@ -9,7 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * The View Holder will be used to hold a reference to each View from the Earthquake item layout.
@@ -19,6 +23,9 @@ import java.util.List;
 public class EarthquakeRecyclerViewAdapter extends RecyclerView.Adapter<EarthquakeRecyclerViewAdapter.ViewHolder> {
 
     private final List<Earthquake> mEarthquake;
+
+    private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm", Locale.US);
+    private static final NumberFormat MAGNITUDE_FORMAT = new DecimalFormat("0.0");
 
     public EarthquakeRecyclerViewAdapter(List<Earthquake> earthquake) {
         this.mEarthquake = earthquake;
@@ -33,8 +40,14 @@ public class EarthquakeRecyclerViewAdapter extends RecyclerView.Adapter<Earthqua
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.earthquake = mEarthquake.get(position);
-        holder.detailsView.setText(mEarthquake.get(position).toString());
+        //holder.earthquake = mEarthquake.get(position);
+        //holder.detailsView.setText(mEarthquake.get(position).toString());
+
+        Earthquake earthquake = mEarthquake.get(position);
+        holder.date.setText(TIME_FORMAT.format(earthquake.getDate()));
+        holder.details.setText(earthquake.getDetails());
+        holder.magnitude.setText(MAGNITUDE_FORMAT.format(earthquake.getMagnitude()));
+
     }
 
 
@@ -45,20 +58,29 @@ public class EarthquakeRecyclerViewAdapter extends RecyclerView.Adapter<Earthqua
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public final  View parentView;
-        public final TextView detailsView;
-        public Earthquake earthquake;
+        //public final  View parentView;
+        //public final TextView detailsView;
+        //public Earthquake earthquake;
+
+        public final TextView date;
+        public final TextView details;
+        public final TextView magnitude;
+
 
         public ViewHolder(@NonNull View view) {
             super(view);
-            parentView = view;
-            detailsView = view.findViewById(R.id.list_item_earthquake_details);
+            //parentView = view;
+            //detailsView = view.findViewById(R.id.list_item_earthquake_details);
+            date = view.findViewById(R.id.date);
+            details = view.findViewById(R.id.details);
+            magnitude = view.findViewById(R.id.magnitude);
         }
 
-        @Override
+        /*@Override
         public String toString() {
+
             return super.toString() + " '" + detailsView.getText() + "'";
-        }
+        }*/
     }
 }
 
