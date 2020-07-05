22633fd9f792a771b89415940951fa40e35ae4bb
JAbozarOid <araghibdoust@hotmail.com>
2020-07-05 17:58:13 +0430

    add json parser to EarthquakeViewModel-99.04.15

--- app/src/main/java/com/aratel/earthquake/viewmodel/EarthquakeViewModel.java
+++ app/src/main/java/com/aratel/earthquake/viewmodel/EarthquakeViewModel.java
@@ -3,6 +3,7 @@ package com.aratel.earthquake.viewmodel;
 import android.app.Application;
 import android.location.Location;
 import android.os.AsyncTask;
+import android.util.JsonReader;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -20,6 +21,7 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -147,7 +149,7 @@ public class EarthquakeViewModel extends AndroidViewModel {
                     Log.e(TAG, "IOException", e);
                 } catch (ParserConfigurationException e) {
                     Log.e(TAG, "Parser Configuration Exception", e);
-                }catch (SAXException e){
+                } catch (SAXException e) {
                     Log.e(TAG, "SAX Exception", e);
                 }
                 // return our result array
@@ -161,5 +163,138 @@ public class EarthquakeViewModel extends AndroidViewModel {
             }
         }.execute();
 
+    }
+
+    private List<Earthquake> parseJson(InputStream in) throws IOException {
+        // Create a new Json Reader to parse the input.
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+        try {
+            // Create an empty list of earthquakes.
+            List<Earthquake> earthquakes = null;
+            // The root node of the Earthquake JSON feed is an object that
+            // we must parse.
+            reader.beginObject();
+            while (reader.hasNext()) {
+                String name = reader.nextName();
+                // We are only interested in one sub-object: the array of
+                // earthquakes labeled as features.
+                if (name.equals("features")) {
+                    earthquakes = readEarthquakeArray(reader);
+                } else {
+                    // We will ignore all other root level values and objects.
+                    reader.skipValue();
+                }
+            }
+            reader.endObject();
+            return earthquakes;
+        } finally {
+            reader.close();
+        }
+    }
+
+    // Traverse the array of earthquakes.
+    private List<Earthquake> readEarthquakeArray(JsonReader reader) throws IOException {
+        List<Earthquake> earthquakes = new ArrayList<Earthquake>();
+        // The earthquake details are stored in an array.
+        reader.beginArray();
+        while (reader.hasNext()) {
+            // Traverse the array, parsing each earthquake.
+            earthquakes.add(readEarthquake(reader));
+        }
+        reader.endArray();
+        return earthquakes;
+    }
+
+    // Parse each earthquake object within the earthquake array.
+    public Earthquake readEarthquake(JsonReader reader) throws IOException {
+        String id = null;
+        Location location = null;
+        Earthquake earthquakeProperties = null;
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("id")) {
+                // The ID is stored as a value.
+                id = reader.nextString();
+            } else if (name.equals("geometry")) {
+                // The location is stored as a geometry object
+                // that must be parsed.
+                location = readLocation(reader);
+            } else if (name.equals("properties")) {
+                // Most of the earthquake details are stored as a
+                // properties object that must be parsed.
+                earthquakeProperties = readEarthquakeProperties(reader);
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        // Construct a new Earthquake based on the parsed details.
+        return new Earthquake(id,
+                earthquakeProperties.getDate(),
+                earthquakeProperties.getDetails(),
+                location,
+                earthquakeProperties.getMagnitude(),
+                earthquakeProperties.getLink());
+    }
+
+    // Parse the properties object for each earthquake object
+    // within the earthquake array.
+    public Earthquake readEarthquakeProperties(JsonReader reader) throws IOException {
+        Date date = null;
+        String details = null;
+        double magnitude = -1;
+        String link = null;
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("time")) {
+                long time = reader.nextLong();
+                date = new Date(time);
+            } else if (name.equals("place")) {
+                details = reader.nextString();
+            } else if (name.equals("url")) {
+                link = reader.nextString();
+            } else if (name.equals("mag")) {
+                magnitude = reader.nextDouble();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return new Earthquake(null, date, details, null, magnitude, link);
+    }
+
+    // Parse the coordinates object to obtain a location.
+    private Location readLocation(JsonReader reader) throws IOException {
+        Location location = null;
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("coordinates")) {
+                // The location coordinates are stored within an
+                // array of doubles.
+                List<Double> coords = readDoublesArray(reader);
+                location = new Location("dummy");
+                location.setLatitude(coords.get(0));
+                location.setLongitude(coords.get(1));
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return location;
+    }
+
+    // Parse an array of doubles.
+    public List<Double> readDoublesArray(JsonReader reader) throws IOException {
+        List<Double> doubles = new ArrayList<Double>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            doubles.add(reader.nextDouble());
+        }
+        reader.endArray();
+        return doubles;
     }
 }
